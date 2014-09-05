5e72b45892fbf4e1713c39712b9ce5b85215d6a3
SIDMAR.MAL@gmail.com <SIDMAR.MAL@gmail.com@31375b71-2890-0b6b-07fe-a48141b9623b>
2014-09-05 00:00:36 +0000

    metodos para registrar entrada e saida de hospedes e calcular as diarias devidas

--- trunk/ atps-poo-tads2014 --username SIDMAR.MAL@gmail.com/ATPS_POO/src/tads_atps_poo/Diaria.java
+++ trunk/ atps-poo-tads2014 --username SIDMAR.MAL@gmail.com/ATPS_POO/src/tads_atps_poo/Diaria.java
@@ -19,7 +19,7 @@ public class Diaria {
 			dataEntrada = data.parse(entrada); // recebe a data da entrada do hospede
 			dataSaida   = data.parse(saida);   // revebe a data de saida do hospede
 		}catch(java.text.ParseException e){    // lança uma exceção caso não seja possivel converter a data
-			return;
+			return 0;
 			}
 		tempoMS    = dataSaida.getTime() - dataEntrada.getTime();  // efetua o calculo do tempo de hospedagem em milisegundos
 		tempoSeg   = tempoMS/1000;                                 // efetua o calcilo do tempo de hospedagem em segundos
@@ -27,7 +27,7 @@ public class Diaria {
 		tempoHoras = tempoMin/60;                                  // efetua o calcilo do tempo de hospedagem em horas 
 		diarias    = (int) tempoHoras/24;                          // efetua o calcilo do tempo de hospedagem em dias
 		
-		if(diarias == 0)     /* não rxidte diaria 0, mesmo que o hospede permaneça */
+		if(diarias == 0)     /* não existe diaria 0, mesmo que o hospede permaneça */
 		return diarias + 1;  /* menos de 24h, ele paga uma diaria completa*/
 		else
 		return diarias;	
--- trunk/ atps-poo-tads2014 --username SIDMAR.MAL@gmail.com/ATPS_POO/src/tads_atps_poo/Quarto.java
+++ trunk/ atps-poo-tads2014 --username SIDMAR.MAL@gmail.com/ATPS_POO/src/tads_atps_poo/Quarto.java
@@ -8,14 +8,18 @@ public class Quarto {
 	private boolean ocupado;
 	private float precoDiaria;
 	private int diarias;
-	public String nomeHospede;
 	private int camaExtra;
-	public Hospede hospede;
+	/* Objeto do tipo Hospede, a classe Quarto tem um
+	 * relacionamento do tipo TEM-UM com a classe Hospede*/ 
+	private Hospede hospede;
+	/* Objeto do tipo Diaria, a classe Quarto tem um
+	 * relacionamento do tipo TEM-UM com a classe Diaria*/ 
+	private Diaria  diaria;
+	
 	
 	        // Construtor da classe, inicializa os atributos
-			public Quarto(String nomeHospede, int numQuarto, int camaExtra)
+			public Quarto(int numQuarto, int camaExtra)
 			{
-				this.nomeHospede = nomeHospede;
 				this.numQuarto = numQuarto;
 				this.camaExtra = camaExtra;
 				}
@@ -42,9 +46,9 @@ public class Quarto {
 				this.tipoQuarto = tipoQuarto;
 			}
 			
-			// Métodos da classe
+			// Métodos da classe, testa se o quarto esta vazio
 			public void quartoVazio(){
-				if((nomeHospede == null)||(nomeHospede == ""))
+				if(isOcupado() != true)
 					setOcupado(false);
 				else
 					setOcupado(true);
