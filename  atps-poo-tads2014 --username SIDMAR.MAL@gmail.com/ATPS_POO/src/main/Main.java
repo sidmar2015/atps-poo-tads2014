4a8750551bc10d37c10f9c96bc7c7624dd44802d
SIDMAR.MAL <SIDMAR.MAL@gmail.com>
2014-09-05 00:00:36 +0000

    metodos para registrar entrada e saida de hospedes e calcular as diarias devidas

---  atps-poo-tads2014 --username SIDMAR.MAL@gmail.com/ATPS_POO/src/main/Main.java
+++  atps-poo-tads2014 --username SIDMAR.MAL@gmail.com/ATPS_POO/src/main/Main.java
@@ -6,39 +6,79 @@ import java.util.Collection;
 
 import javax.swing.*;
 
+import java.util.Scanner;
+
 public class Main {
+	static Quarto[] acomodacao = new Quarto[60];
 	public static void main(String[] args){
-		/* Instancia da classe hospede*/
-		Hospede hospede = new Hospede();
-		
-		/* Instancia da classe diaria*/
-		Diaria diaria = new Diaria();
-		
+			
 		/* Instancia dos 60 quartos*/
-		Quarto[] acomodacao = new Quarto[60]; 
-		for(int i=0;i<60;i++){
-			acomodacao[i] = new Quarto(null,i,0);
+			for(int i=0;i<60;i++){
+			acomodacao[i] = new Quarto(i,0);
 			if(i <= 50)
 				acomodacao[i].setTipoQuarto(acomodacao[i].getTipoQuarto().COMUM); // De 1 - 50 quartos comuns
 			if(i >= 60)
 				acomodacao[i].setTipoQuarto(acomodacao[i].getTipoQuarto().CHALE); // De 51 - 60 chales
 		} // Fim do laço for
-		System.out.println(acomodacao[1].numQuarto +"\n"
-		+ acomodacao[1].nomeHospede +"\n"+ acomodacao[1].getTipoQuarto());
+		
 	} // Fim do main
 	
-	public class MenuPrincipal{
+	
 		
-		public void cadastrar(){
-				
-		}
+		public void registrarEntrada(){
+			Scanner s = new Scanner(System.in);
+			System.out.println("Digite o numero do quarto");
+			int nQuarto = s.nextInt();
+			    /* testa se o quarto esta vazio*/
+				if(acomodacao[nQuarto].isOcupado == true){
+					System.out.println("O quarto "+ nQuarto +" Esta ocupado");
+					return ;
+				} // fim if
+				else{
+					System.out.println("Digite o nome do hospede");
+					acomodacao[nQuarto].hospede.setNome(s.next());
+					System.out.println("Digite o numero de documento do hospede");
+					acomodacao[nQuarto].hospede.setNumDocumento(s.next());
+					System.out.println("Digite a data de entada dd/mm/aaaa");
+					acomodacao[nQuarto].hospede.setDataEntrada(s.next());
+				} // fim else
+		} // fim metodo
+		
 		public void menu(){
 			
 		}
-		public void fecharConta(){
+		public void registrarSaida(){
+			Scanner s = new Scanner(System.in);
+			System.out.println("Digite o numero do quarto");
+			int numQuarto = s.nextInt();
+			if(acomodacao[nQuarto].isOcupado == false){
+				System.out.println("O quarto "+ nQuarto +" Esta vazio");
+			} // fim if
+			else{
+				System.out.println("Deseja fechar a conta do quarto "+nQuarto+"? <1> SIM <2> NAO");
+				int opcao = s.nextInt();
+				
+				switch(opcao){
+				case 1:
+					
+					int tDiarias = acomodacao[nQuarto].diaria.calculaDiarias(acomodacao[nQuarto].hospede.getDataEntrada(),
+							acomodacao[nQuarto].hospede.getDataSaida());
+					System.out.println("Tempo de permanencia do Sr(a) "+acomodacao[nQuarto].hospede.getNome()+"\n"
+							+tDiarias+" dias, data de entrada "+acomodacao[nQuarto].hospede.getDataEntrada()+
+							" data de saida "+acomodacao[nQuarto].hospede.getDataSaida()+
+							"\n Total a pagar: "+(tDiarias*acomodacao[nQuarto].getPrecoDiaria()));
+				case 2:
+					return;
+				default:
+					return;
+				}
+				
+				
+			}
+			
 			
 		}
 		
-	}
+	
 
 }
